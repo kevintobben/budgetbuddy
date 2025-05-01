44125344a395a7a76977508b1117874bd790457e
Kevin Tobben <kevintobben@outlook.com>
2025-05-01 10:52:48 +0200

    Uitlijning nav bar table

--- src/pages/Income.tsx
+++ src/pages/Income.tsx
@@ -99,64 +99,100 @@ const Income: React.FC = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 place-items-center pb-12">
         <OverviewCard
           title="Aantal inkomst bronnen"
-          amount="€ 1600,00"
-          amountColor="text-green-600"
+          amount="4"
         />
         <OverviewCard
           title="Aatanl inkomen in €"
           amount="€ 1.120,00"
-          amountColor="text-red-500"
+          amountColor="text-green-500"
         />
       </div>
 
-      <div className="flex flex-wrap items-center justify-between pb-4">
+      <div className="flex flex-wrap items-center justify-between gap-4 pb-4">
+        <div className="flex items-center gap-2">
         <Input type="text" placeholder="Zoek op naam" className="w-64" />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="sm">
-              Sorteer
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>Sorteer op</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Dag</DropdownMenuItem>
-            <DropdownMenuItem>Week</DropdownMenuItem>
-            <DropdownMenuItem>Maand</DropdownMenuItem>
-            <DropdownMenuItem>Jaar</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline">Open</Button>
+            <Button variant="outline">Categorie</Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent className="w-56">
-            <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+            {/* <Input type="text" placeholder="Zoek op naam" className="" /> */}
             <DropdownMenuSeparator />
             <DropdownMenuCheckboxItem
               checked={showStatusBar}
               onCheckedChange={setShowStatusBar}
             >
-              Status Bar
+              Salaris
             </DropdownMenuCheckboxItem>
             <DropdownMenuCheckboxItem
               checked={showActivityBar}
               onCheckedChange={setShowActivityBar}
-              disabled
             >
-              Activity Bar
+              Zakgeld
             </DropdownMenuCheckboxItem>
             <DropdownMenuCheckboxItem
               checked={showPanel}
               onCheckedChange={setShowPanel}
             >
-              Panel
+              Anders
             </DropdownMenuCheckboxItem>
           </DropdownMenuContent>
         </DropdownMenu>
-      </div>
+        </div>
 
+        <div className="flex items-center gap-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="sm">
+                Sorteer
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Sorteer op</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Dag</DropdownMenuItem>
+              <DropdownMenuItem>Week</DropdownMenuItem>
+              <DropdownMenuItem>Maand</DropdownMenuItem>
+              <DropdownMenuItem>Jaar</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">Open</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuCheckboxItem
+                checked={showStatusBar}
+                onCheckedChange={setShowStatusBar}
+              >
+                Naam
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={showActivityBar}
+                onCheckedChange={setShowActivityBar}
+              >
+                Prijs
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={showPanel}
+                onCheckedChange={setShowPanel}
+              >
+                Datum
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={showPanel}
+                onCheckedChange={setShowPanel}
+              >
+                Categorie
+              </DropdownMenuCheckboxItem>
+              
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
 
       <BaseTable columns={incomeColumns} data={incomeData} />
     </PageLayout>
